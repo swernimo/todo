6650285ceda46d5f14172f147811e5ed7462d7d6
Sean Wernimont <swernimo@users.noreply.github.com>
2024-03-25 09:51:17 -0600

    added health controller

---
+++ API/API/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class HealthController : ControllerBase
+  {
+    [HttpGet]
+    [Route("ping")]
+    public IActionResult Ping()
+    {
+      return Ok("");
+    }
+  }
+}
