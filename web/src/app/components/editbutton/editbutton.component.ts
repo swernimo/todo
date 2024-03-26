49720359370fa1c9618290f73dc507799a9c7d16
Sean Wernimont <swernimo@users.noreply.github.com>
2024-03-26 13:11:55 -0600

    edit tooltip

--- web/src/app/components/editbutton/editbutton.component.ts
+++ web/src/app/components/editbutton/editbutton.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-editbutton',
   standalone: true,
-  imports: [],
+  imports: [MatTooltipModule, CommonModule],
   templateUrl: './editbutton.component.html',
   styleUrl: './editbutton.component.css'
 })
 export class EditbuttonComponent {
 
+  @Input()
+  public tooltip: string = '';
+
+  @Output()
+  public editBtnClicked: EventEmitter<void> = new EventEmitter();
+
+  constructor() {}
+
+
+  public clicked(): void {
+    this.editBtnClicked.emit();
+  }
 }
